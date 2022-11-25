4d866eeb4a885fadae33da0f4f19964259bc59a4
mintmaya <118422299+mintmaya@users.noreply.github.com>
2022-11-25 16:52:26 +0900

    Initial commit

---
+++ 25.11/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _25._11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse("10");
+            double b = double.Parse("23,56");
+            decimal c = decimal.Parse("12,45");
+            byte d = byte.Parse("4");
+            Console.WriteLine($"a={a}  b={b}  c={c}  d={d}");
+
+
+
+            Console.WriteLine("Введите строку:");
+            string input = Console.ReadLine();
+
+            bool result = int.TryParse(input, out var number);
+            if (result == true)
+                Console.WriteLine($"Преобразование прошло успешно. Число: {number}");
+            else
+                Console.WriteLine("Преобразование завершилось неудачно");
+        }
+    }
+}
